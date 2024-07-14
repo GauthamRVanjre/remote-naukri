55313f3239a5f3f867fd59858cc1455225202d78
Gautham Vanjre <vanjregautham@gmail.com>
2024-07-14 19:25:05 +0530

    feat: google adsense ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,12 @@ export default function RootLayout({
       <meta
         name="google-site-verification"
         content="MDKmyGrPdT0K6FE8MIEs-WrbTo-NAA-2t_kYsBI13KY"
+      />
+      <meta name="google-adsense-account" content="ca-pub-4511349409485589" />
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4511349409485589"
+        crossOrigin="anonymous"
       />
       <Analytics />
       <body className={inter.className}>{children}</body>
